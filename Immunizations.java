befa778618f4ef1b4c8617dde1513c7206ed8dd8
kategator <guthkate@gmail.com>
2016-03-28 23:38:41 -0400

    Create Immunizations.java

---
+++ Immunizations.java
@@ -0,0 +1,13 @@
+package com.gmail.kate.guth.pediatrics;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Immunizations extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_immunizations);
+    }
+}
